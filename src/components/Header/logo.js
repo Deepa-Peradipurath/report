582d0b6264067606ca03922c6c6741aba1349afd
Deepa-Peradipurath <deepa.peradipurath@gmail.com>
2020-01-27 16:06:30 +0530

    Add files via upload

--- src/components/Header/logo.js
+++ src/components/Header/logo.js
@@ -7,7 +7,7 @@ import { makeStyles } from '@material-ui/core/styles'
 const useStyles = makeStyles(theme => ({
     logo: {
         maxWidth: '100%',
-        margin:'-13`px 10px 0 0'
+        margin:'-13px 10px 0 0'
       } 
 }));
 
