502168123a22bbe82de35d8692be2a37aab4d338
Deepa-Peradipurath <deepa.peradipurath@gmail.com>
2020-01-27 15:35:32 +0530

    Add files via upload

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,7 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import AppBar from './AppBar';
+import AppBar from './AppBar-old';
 
 const useStyles = makeStyles(theme => ({
   grow: {
---
+++ src/pages/index.js
@@ -0,0 +1,39 @@
+import React from "react"
+import {Provider} from "react-redux"
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'; 
+
+import Layout from "../components/layout"
+//import Image from "../components/image"
+import SEO from "../components/seo"
+import { store } from "../store" 
+
+const theme = createMuiTheme({
+  palette: {
+     primary: {
+        light: '#fff',
+        main: 'rgb(15, 54, 84)',
+        dark: '#000'
+     },
+     secondary: {
+       main: '#f44336',
+     },
+  },
+  typography: { 
+     useNextVariants: true
+  }
+});
+
+const IndexPage = () => (
+  <Provider store={ store }>
+    <Layout>
+      <SEO title="Home" />
+      <MuiThemeProvider theme = { theme }>
+      <h1>UI-Admin Notification Report Landing Page</h1>
+      </MuiThemeProvider> 
+      
+    </Layout>
+  </Provider>
+  
+)
+
+export default IndexPage
